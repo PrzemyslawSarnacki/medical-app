155c4b841fae45f02dbc20526b1222b6787b9905
PrzemyslawSarnacki <przemos100@gmail.com>
2021-01-30 19:03:11 +0100

    UI for BF

--- src/main/java/wel/medicalapp/ui/BodyFatCalculatorView.java
+++ src/main/java/wel/medicalapp/ui/BodyFatCalculatorView.java
@@ -7,40 +7,55 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-import wel.medicalapp.service.BmiCalculator;
 import wel.medicalapp.service.BodyFatCalc;
 
 @Route("ui/bodyfat")
-public class BodyFatCalculatorView {
+public class BodyFatCalculatorView extends VerticalLayout{
     @Autowired
     private BodyFatCalc calculator;
 
     public BodyFatCalculatorView() {
-        TextField weight = new TextField("Weight:");
         Label infoLabel = new Label("Welcome in the label");
+        TextField neck = new TextField("Neck:");
+        TextField waist = new TextField("Waist:");
         TextField height = new TextField("Height:");
         Label label = new Label("Result");
-        Select<String> labelSelect = new Select<>();
-        labelSelect.setItems("male", "female");
-        labelSelect.setLabel("Gender");
+        Select<String> gender = new Select<>();
+        gender.setItems("male", "female");
+        gender.setLabel("Gender");
         TextField result = new TextField("BodyFat:");
         result.setEnabled(false);
         Button calculate = new Button("Calculate");
 
         calculate.addClickListener((ComponentEventListener<ClickEvent<Button>>) event -> {
             try {
-                String bodyfat = calculator.calculate(weight.getValue(), height.getValue());
-                result.setValue(bmi);
+                String bodyfat = calculator.calculate(gender.getValue(), height.getValue(), waist.getValue(), neck.getValue());
+                result.setValue(bodyfat);
             } catch (Exception e) {
                 Notification notification = new Notification("Error occured");
                 notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
                 notification.open();
             }
         });
+
+        Button myResultsButton = new Button("Show my results");
+
+        myResultsButton.addClickListener((ComponentEventListener<ClickEvent<Button>>) event -> {
+            myResultsButton.getUI().ifPresent(ui ->
+                    ui.navigate("ui/bodyfatresults"));
+
+        });
+        VerticalLayout layout = new VerticalLayout();
+        layout.add(infoLabel, neck, height, label,waist, calculate, result, myResultsButton);
+        layout.setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        add(layout);
 
 
     }
--- src/main/java/wel/medicalapp/ui/BodyFatResultsView.java
+++ src/main/java/wel/medicalapp/ui/BodyFatResultsView.java
@@ -1,4 +1,46 @@
 package wel.medicalapp.ui;
 
-public class BodyFatResultsView {
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import wel.medicalapp.model.Bmi;
+import wel.medicalapp.model.BodyFat;
+import wel.medicalapp.service.BmiCalculator;
+import wel.medicalapp.service.BodyFatCalc;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Route("ui/bodyfatresults")
+public class BodyFatResultsView extends VerticalLayout {
+    private final Grid<BodyFat> grid = new Grid<>(BodyFat.class);
+
+    @Autowired
+    private BodyFatCalc calculator;
+
+    public BodyFatResultsView() {
+        setMargin(true);
+        setSizeFull();
+
+        grid.setSizeFull();
+        grid.setColumnReorderingAllowed(true);
+
+        add(grid);
+
+        setHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        setAlignItems(FlexComponent.Alignment.CENTER);
+        add(grid);
+
+    }
+
+    @PostConstruct
+    public void init() {
+        List<BodyFat> allResults = calculator.getAllResults();
+        grid.setDataProvider(new ListDataProvider<>(allResults));
+    }
+
 }
+
